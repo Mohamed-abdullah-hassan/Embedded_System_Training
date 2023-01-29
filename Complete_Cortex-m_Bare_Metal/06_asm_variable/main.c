88e756003188875318eb8cee731def258f0dcd72
Mohamed Abdullah Hassan <eng.moh.a.hassan@gmail.com>
2023-01-29 08:31:12 +0200

    Typo correct

--- Complete_Cortex-m_Bare_Metal/06_asm_variable/main.c
+++ Complete_Cortex-m_Bare_Metal/06_asm_variable/main.c
@@ -1,4 +1,4 @@
-// Demo for using external variable created by assembly file
+// Demo for using external variable created in assembly file
 
 extern unsigned int amount;             // Telling the compiler that there is a variable called "amount" which is created outside the c file and then defiening its datatype
 unsigned int myFunc(void);
